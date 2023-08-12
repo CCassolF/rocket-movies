b3337ab79d392c6a418e35f3d70fe928b44cabb2
carlos <carloscassolfilho@gmail.com>
2023-08-12 03:40:08 -0300

    MoviePreview page layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { NewMovie } from './pages/NewMovie'
+import { MoviePreview } from './pages/MoviePreview'
 
 import './styles/global.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NewMovie />
+    <MoviePreview />
   </React.StrictMode>,
 )
